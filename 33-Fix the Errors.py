f6702a17061a83ee10937e427ef9aa3ee7e4ddaf
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2025-03-01 13:10:01 +0300

    Add files via upload

---
+++ 33-Fix the Errors.py
@@ -0,0 +1,8 @@
+try:
+    age = int(input("How old are you?   "))
+except ValueError:
+    print("You have typed in an invalid number. Use numerical number.")
+    age = int(input("How old are you?   "))
+
+if age > 18:
+    print(f"You can drive at age {age}.")
